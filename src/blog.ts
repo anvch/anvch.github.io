f8f462d038c5daef07cf87d638f4023be3705212
Angela Chen <angelachen.v@gmail.com>
2023-10-22 19:35:25 -0700

    feat: create TS type Blog and list with two blog objects

---
+++ src/blog.ts
@@ -0,0 +1,21 @@
+type Blog = {
+  title: string;
+  date: string;
+  description: string;
+  slug: string;
+};
+
+const blog: Blog[] = [
+  {
+    title: "The Intersection of Arts and Computing",
+    date: "October 23, 2023",
+    description: "A brief article on virtual, augmented, and mixed reality.",
+    slug: "url",
+  },
+  {
+    title: "What My Dance Journey Has Taught Me",
+    date: "October 23, 2023",
+    description: "Summarizing my dance journey and what lessons I've learned.",
+    slug: "url",
+  },
+];
